6ee28ba401dc0f3879cfd0d5370bf7c51e7edd75
Brent Hughes <brentahughes@gmail.com>
2016-05-12 17:16:11 -0500

    Updated the application object to use the get method so it will default without error

--- roku/core.py
+++ roku/core.py
@@ -207,8 +207,8 @@ class Roku(object):
             return None
 
         return Application(
-            id=app_node.attrib['id'],
-            version=app_node.attrib['version'],
+            id=app_node.get('id'),
+            version=app_node.get('version'),
             name=app_node.text,
             roku=self,
         )
