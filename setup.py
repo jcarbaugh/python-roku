c4a3c64bcea85c5f2157e84bb8b2ec88ae7de0de
Brent Hughes <bah2830@users.noreply.github.com>
2017-01-20 22:42:47 -0600

    Update version to 3.1.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ f.close()
 
 setup(
     name='roku',
-    version='3.1.2',
+    version='3.1.3',
     description='Client for the Roku media player',
     long_description=readme,
     author='Jeremy Carbaugh',
