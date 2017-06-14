590e5b9b337f7aa5a9cf8589c074ab1485ce266c
Brian Schlenker <bschlenk@umich.edu>
2017-06-13 21:54:14 -0700

    Add RokuTV commands
    See https://sdkdocs.roku.com/display/sdkdoc/External+Control+API#ExternalControlAPI-KeypressKeyValues
    for a list of all supported commands.

--- roku/core.py
+++ roku/core.py
@@ -17,6 +17,7 @@ __version__ = '3.0.0'
 
 
 COMMANDS = {
+    # Standard Keys
     'home': 'Home',
     'reverse': 'Rev',
     'forward': 'Fwd',
@@ -33,6 +34,28 @@ COMMANDS = {
     'search': 'Search',
     'enter': 'Enter',
     'literal': 'Lit',
+
+    # For devices that support "Find Remote"
+    'find_remote': 'FindRemote',
+
+    # For Roku TV
+    'volume_down': 'VolumeDown',
+    'volume_up': 'VolumeUp',
+    'volume_mute': 'VolumeMute',
+
+    # For Roku TV while on TV tuner channel
+    'channel_up': 'ChannelUp',
+    'channel_down': 'ChannelDown',
+
+    # For Roku TV current input
+    'input_tuner': 'InputTuner',
+    'input_hdmi1': 'InputHDMI1',
+    'input_hdmi2': 'InputHDMI2',
+    'input_hdmi3': 'InputHDMI3',
+    'input_hdmi4': 'InputHDMI4',
+    'input_av1': 'InputAV1',
+
+    # For devices that support being turned on/off
     'power': 'Power',
 }
 
