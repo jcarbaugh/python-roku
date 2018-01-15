c34e4a628201fed6cbacc7c7fe33efa443a73a25
Spencer Oberstadt <soberstadt@gmail.com>
2018-01-14 21:24:59 -0500

    Update core.py to match upsteam (#12)
    jcarbaugh has added a few new commands to his list, and it would be great to have them available!
    https://github.com/jcarbaugh/python-roku/blob/5b12c15777baf77424b525bdd469509906c13ee7/roku/core.py

--- roku/core.py
+++ roku/core.py
@@ -11,6 +11,7 @@ __version__ = '3.1.4'
 roku_logger = logging.getLogger('roku')
 
 COMMANDS = {
+    # Standard Keys
     'home': 'Home',
     'reverse': 'Rev',
     'forward': 'Fwd',
@@ -27,9 +28,29 @@ COMMANDS = {
     'search': 'Search',
     'enter': 'Enter',
     'literal': 'Lit',
+
+    # For devices that support "Find Remote"
+    'find_remote': 'FindRemote',
+
+    # For Roku TV
     'volume_down': 'VolumeDown',
-    'volume_mute': 'VolumeMute',
     'volume_up': 'VolumeUp',
+    'volume_mute': 'VolumeMute',
+
+    # For Roku TV while on TV tuner channel
+    'channel_up': 'ChannelUp',
+    'channel_down': 'ChannelDown',
+
+    # For Roku TV current input
+    'input_tuner': 'InputTuner',
+    'input_hdmi1': 'InputHDMI1',
+    'input_hdmi2': 'InputHDMI2',
+    'input_hdmi3': 'InputHDMI3',
+    'input_hdmi4': 'InputHDMI4',
+    'input_av1': 'InputAV1',
+
+    # For devices that support being turned on/off
+    'power': 'Power'
 }
 
 SENSORS = ('acceleration', 'magnetic', 'orientation', 'rotation')
