c59c69fe061cf7952c4311b4e6cf4e9f9145295f
Jeremy Carbaugh <39159+jcarbaugh@users.noreply.github.com>
2019-10-04 17:15:15 -0400

    add scripting engine

---
+++ roku/scripting.py
@@ -0,0 +1,50 @@
+import logging
+import os
+import re
+import time
+from collections import namedtuple
+
+SCRIPT_RE = re.compile(r"(?P<command>\w+)(?:\:(?P<param>[\w\s]+))?(?:\@(?P<count>\d+))?(?:\*(?P<sleep>[\d\.]+))?")  # noqa
+
+Command = namedtuple('Command', ['command', 'param', 'count', 'sleep'])
+
+logger = logging.getLogger('roku.scripting')
+
+
+def load_script(path, params=None, raw=False):
+    if not os.path.exists(path):
+        raise ValueError(f'script at {path} not found')
+    with open(path) as infile:
+        content = infile.read()
+    if params:
+        content = content.format(**params)
+    if not raw:
+        content = content.strip().split('\n')
+    return content
+
+
+def parse_script(script):
+    commands = []
+    for line in script:
+        if not line:
+            continue
+        m = SCRIPT_RE.match(line)
+        if m:
+            data = m.groupdict()
+            data['count'] = int(data['count'] or 1)
+            data['sleep'] = float(data['sleep']) if data['sleep'] else None
+            commands.append(Command(**data))
+    return commands
+
+
+def run_script(roku, script, sleep=0.5):
+    for cmd in script:
+        logger.debug(cmd)
+        for i in range(cmd.count or 1):
+            func = getattr(roku, cmd.command)
+            if func:
+                if cmd.param:
+                    func(cmd.param)
+                else:
+                    func()
+                time.sleep(cmd.sleep or sleep)
