14501421efaab2f8b79e1c72473eaa4c08e4107f
Brett T. Warden <4c0e8e88@tm.wgz.org>
2019-11-26 16:24:51 -0800

    Implement PowerOn command
    Add a command to send the PowerOn keypress event, alongside the
    existing Power and PowerOff events.
    
    Fixes #52

--- roku/core.py
+++ roku/core.py
@@ -58,6 +58,7 @@ COMMANDS = {
     # For devices that support being turned on/off
     'power': 'Power',
     'poweroff': 'PowerOff',
+    'poweron': 'PowerOn',
 }
 
 SENSORS = ('acceleration', 'magnetic', 'orientation', 'rotation')
