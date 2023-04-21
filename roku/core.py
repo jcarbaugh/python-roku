1edddaecb200632f303ba687fb3fc6e718579472
Ronnie Llamado <35092029+ronnie-llamado@users.noreply.github.com>
2023-04-21 15:15:55 -0700

    Define `__dir__` to contain `COMMANDS` and `SENSORS` (#76)
    * Update __dir__ to contain Roku.commands
    
    * Add sensors to Roku.__dir__
    
    * Add attributes back into __dir__

--- roku/core.py
+++ roku/core.py
@@ -209,6 +209,14 @@ class Roku(object):
             app = self._app_for_id(key)
         return app
 
+    def __dir__(self):
+        return sorted(
+            dir(type(self)) + 
+            list(self.__dict__.keys()) + 
+            list(COMMANDS.keys()) + 
+            list(SENSORS)
+        )
+
     def _app_for_name(self, name):
         for app in self.apps:
             if app.name == name:
