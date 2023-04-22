b88125abca91573f4882d1265a38490a4bbff8ba
Jeremy Carbaugh <39159+jcarbaugh@users.noreply.github.com>
2023-04-21 18:13:09 -0600

    black formatting

--- roku/scripting.py
+++ roku/scripting.py
@@ -4,22 +4,24 @@ import re
 import time
 from collections import namedtuple
 
-SCRIPT_RE = re.compile(r"(?P<command>\w+)(?:\:(?P<param>[\w\s]+))?(?:\@(?P<count>\d+))?(?:\*(?P<sleep>[\d\.]+))?")  # noqa
+SCRIPT_RE = re.compile(
+    r"(?P<command>\w+)(?:\:(?P<param>[\w\s]+))?(?:\@(?P<count>\d+))?(?:\*(?P<sleep>[\d\.]+))?"
+)  # noqa
 
-Command = namedtuple('Command', ['command', 'param', 'count', 'sleep'])
+Command = namedtuple("Command", ["command", "param", "count", "sleep"])
 
-logger = logging.getLogger('roku.scripting')
+logger = logging.getLogger("roku.scripting")
 
 
 def load_script(path, params=None, raw=False):
     if not os.path.exists(path):
-        raise ValueError(f'script at {path} not found')
+        raise ValueError(f"script at {path} not found")
     with open(path) as infile:
         content = infile.read()
     if params:
         content = content.format(**params)
     if not raw:
-        content = content.strip().split('\n')
+        content = content.strip().split("\n")
     return content
 
 
@@ -31,8 +33,8 @@ def parse_script(script):
         m = SCRIPT_RE.match(line)
         if m:
             data = m.groupdict()
-            data['count'] = int(data['count'] or 1)
-            data['sleep'] = float(data['sleep']) if data['sleep'] else None
+            data["count"] = int(data["count"] or 1)
+            data["sleep"] = float(data["sleep"]) if data["sleep"] else None
             commands.append(Command(**data))
     return commands
 
--- roku/tests/conftest.py
+++ roku/tests/conftest.py
@@ -4,14 +4,13 @@ from roku import Application, Roku
 
 
 class Fauxku(Roku):
-
     def __init__(self, *args, **kwargs):
         super(Fauxku, self).__init__(*args, **kwargs)
         self._calls = []
 
     def _call(self, method, path, *args, **kwargs):
         self._calls.append((method, path, args, kwargs))
-        return ''
+        return ""
 
     def calls(self):
         return self._calls
@@ -22,15 +21,15 @@ class Fauxku(Roku):
 
 @pytest.fixture
 def roku():
-    return Fauxku('0.0.0.0')
+    return Fauxku("0.0.0.0")
 
 
 @pytest.fixture
 def apps(roku):
     faux_apps = [
-        Application('11', '1.0.1', 'Fauxku Channel Store', roku),
-        Application('22', '2.0.2', 'Faux Netflix', roku),
-        Application('33', '3.0.3', 'Faux YouTube', roku),
-        Application('44HL', '4.0.4', 'Faux Hulu', roku),
+        Application("11", "1.0.1", "Fauxku Channel Store", roku),
+        Application("22", "2.0.2", "Faux Netflix", roku),
+        Application("33", "3.0.3", "Faux YouTube", roku),
+        Application("44HL", "4.0.4", "Faux Hulu", roku),
     ]
     return faux_apps
