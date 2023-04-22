8ab531446c6ac2294eaa5e7b180b9f475cc8710a
Jeremy Carbaugh <39159+jcarbaugh@users.noreply.github.com>
2023-04-21 18:12:08 -0600

    add scripting engine (#48)

---
+++ roku/tests/conftest.py
@@ -0,0 +1,36 @@
+import pytest
+
+from roku import Application, Roku
+
+
+class Fauxku(Roku):
+
+    def __init__(self, *args, **kwargs):
+        super(Fauxku, self).__init__(*args, **kwargs)
+        self._calls = []
+
+    def _call(self, method, path, *args, **kwargs):
+        self._calls.append((method, path, args, kwargs))
+        return ''
+
+    def calls(self):
+        return self._calls
+
+    def last_call(self):
+        return self._calls[-1]
+
+
+@pytest.fixture
+def roku():
+    return Fauxku('0.0.0.0')
+
+
+@pytest.fixture
+def apps(roku):
+    faux_apps = [
+        Application('11', '1.0.1', 'Fauxku Channel Store', roku),
+        Application('22', '2.0.2', 'Faux Netflix', roku),
+        Application('33', '3.0.3', 'Faux YouTube', roku),
+        Application('44HL', '4.0.4', 'Faux Hulu', roku),
+    ]
+    return faux_apps
---
+++ roku/tests/test_scripting.py
@@ -0,0 +1,69 @@
+import pytest
+
+from roku import scripting
+
+
+SCRIPT_PATH = 'roku/tests/scripts/testscript.txt'
+
+
+def test_loading():
+    scripting.load_script(SCRIPT_PATH)
+
+
+def test_loading_params():
+    params = {
+        'avar': 'here',
+        'notavar': 'missing',
+    }
+    content = scripting.load_script(SCRIPT_PATH, params=params, raw=True)
+    assert "literal:here" in content
+    assert "literal:missing" not in content
+
+
+def test_loading_notfound():
+    with pytest.raises(ValueError):
+        scripting.load_script('thisisnotarealscript.txt')
+
+
+def test_parse_command_only():
+    content = ('home',)
+    script = scripting.parse_script(content)
+    command = script[0]
+    assert command == scripting.Command('home', None, 1, None)
+
+
+def test_parse_command_param():
+    content = ('literal:barbecue',)
+    script = scripting.parse_script(content)
+    command = script[0]
+    assert command == scripting.Command('literal', 'barbecue', 1, None)
+
+
+def test_parse_command_count():
+    content = ('left@10',)
+    script = scripting.parse_script(content)
+    command = script[0]
+    assert command == scripting.Command('left', None, 10, None)
+
+
+def test_parse_command_sleep():
+    content = ('left*2',)
+    script = scripting.parse_script(content)
+    command = script[0]
+    assert command == scripting.Command('left', None, 1, 2.0)
+
+
+def test_parse_command_all():
+    content = ('literal:barbecue@3*5.1',)
+    script = scripting.parse_script(content)
+    command = script[0]
+    assert command == scripting.Command('literal', 'barbecue', 3, 5.1)
+
+
+def test_run_script(roku):
+    content = ('home', 'literal:x')
+    script = scripting.parse_script(content)
+    scripting.run_script(roku, script)
+    calls = roku.calls()
+    assert 'keypress/Home' in calls[0][1]
+    assert 'keypress/Lit_x' in calls[1][1]
